950005279688f899d95fccc0945c5ae0351ff4bd
Daniil Artyukhin <d.ar57913@gmail.com>
2025-04-18 21:20:50 +0300

    Yandex CLoud Homework: task 4

--- yandex_cloud_task/yandex_data_proc/DML.sql
+++ yandex_cloud_task/yandex_data_proc/DML.sql
@@ -11,8 +11,8 @@ FROM transactions
 GROUP BY is_fraud;
 
 --3
-SELECT to_date(transaction_date) as transaction_date, COUNT(*) AS transactions_amount,
-SUM(amount) AS total_amount, AVG(amount) AS transactions_avg_amount
+SELECT to_date(transaction_date) as transaction_date, COUNT(*) AS transactions_count,
+SUM(amount) AS total_amount, AVG(amount) AS avg_amount
 FROM transactions
 GROUP BY to_date(transaction_date);
 
@@ -24,10 +24,17 @@ GROUP BY t.transaction_id
 ORDER BY logs_count;
 
 --5
-SELECT DISTINCT l.category,
+SELECT agg.category_name, MAX(agg.transactions_count) AS transactions_count,
+SUM(agg.avg_amount) AS total_amount,
+AVG(agg.avg_amount) AS avg_amount
+FROM (
+SELECT DISTINCT l.category AS category_name,
 (DENSE_RANK() over (partition by category order by t.transaction_id) 
 + DENSE_RANK() over (partition by category order by t.transaction_id DESC) 
-- 1) AS transactions_count
+- 1) AS transactions_count,
+AVG(amount) OVER (partition by t.transaction_id) as avg_amount
 FROM logs AS l
 JOIN transactions AS t ON l.transaction_id=t.transaction_id
-ORDER BY transactions_count DESC;
+ORDER BY category_name DESC
+) AS agg
+GROUP BY agg.category_name;
